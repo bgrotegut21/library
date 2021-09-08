e09094a611efe3b2bae771fa05ce22b19143d7bc
bgrotegut21 <bgrotegut21@gmail.com>
2021-09-08 15:53:39 -0600

    woking on creatring books when you type in information

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 let darkenPage = document.querySelector(".darkenPage");
 let addShelf = document.querySelector(".addShelf");
+let bookShelf = document.querySelector(".bookShelf");
 let titleText = document.querySelector(".titleText");
 let authorText = document.querySelector(".authorText");
 let descriptionText = document.querySelector(".descriptionText");
@@ -13,12 +14,13 @@ let books = [];
 
 let bookHasBeenRead = false;
 
-function Book(title,author,description,totalPages,bookRead){
+function Book(title,author,description,totalPages,bookRead, element){
     this.title = title;
     this.author = author;
     this.description = description;
     this.totalPages = totalPages;
     this.bookRead = bookRead;
+
 }
 
 addBook.addEventListener("click", () => {
@@ -46,12 +48,64 @@ bookRead.addEventListener("click", () => {
     }
 })
 
+
+
+
 doneButton.addEventListener("click", () => {
     let titleValue = titleText.value;
     let authorValue = authorText.value;
     let descriptionValue = descriptionText.value;
     let pagesValue = pagesText.value;
+    let book = new Book(titleValue, authorValue, descriptionValue, pagesValue);
+    books.push(book);
 
+    
 
     
 })
+
+function createNewBook (book,hasRead){
+    let bookDiv = document.createElement("div");
+    bookDiv.setAttribute("class","book");
+
+    let toolHolder = document.createElement("div");
+    toolHolder.setAttribute("class", "toolHolder");
+
+    let bookTools = document.createElement("div");
+    bookTools.setAttribute("class", "bookTools");
+
+    let deleteBook = document.createElement("div");
+    deleteBook.setAttribute("class", "deleteBook");
+
+    let editBook = document.createElement("div");
+    editBook.setAttribute("class", "editBook");
+
+    let textHolder = document.createElement("div");
+    textHolder.setAttribute("class", "textHolder");
+
+    let bookTitle = document.createElement("div");
+    bookTitle.setAttribute("class", "bookTitle");
+    bookTitle.textContent = book.title;
+
+    let author = document.createElement("div");
+    author.setAttribute("class", "author");
+    author.textContent = `by ${book.author}`
+
+    let bookDescription = document.createElement("div");
+    bookDescription.setAttribute("class", "bookDescription");
+    bookDescription.textContent = book.description;
+
+    let bookStatsHolder = document.createElement("div");
+    bookStatsHolder.setAttribute("class", "bookStatsHolder");
+    
+    let bookStats = document.createElement("div");
+    bookStats.setAttribute("class", "bookStats");
+
+    let totalPages = document.createElement("div");
+    totalPages.setAttribute("class", "totalPages");
+    totalPages.textContent = `${book.totalPages} total pages`;
+
+
+
+
+}
